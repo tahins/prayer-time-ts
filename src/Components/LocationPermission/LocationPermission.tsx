21575fcaf94c1174854a9cdbbaa1702b08bdb852
yeaseenur.rahman <yeaseenur.rahman@infolytx.com>
2020-04-16 20:54:23 +0600

    Fix location permission issue

--- src/Components/LocationPermission/LocationPermission.tsx
+++ src/Components/LocationPermission/LocationPermission.tsx
@@ -2,26 +2,37 @@ import React, { useContext } from "react";
 import { PositionContext } from "../../Contexts/position.context";
 import { PlaceContext } from "../../Contexts/place.context";
 import LocationService from "../../Services/location.service";
-import LocationPermissionView from "./LocationPermissionView";
+import { Icon } from 'react-icons-kit';
+import { crosshair } from 'react-icons-kit/feather/crosshair';
+
+import "./LocationPermission.css";
 
 function LocationPermission() {
   const locationService = new LocationService();
   const positionContext = useContext(PositionContext);
   const placeContext = useContext(PlaceContext);
-  const getUserLocationAndPlace = getUserLocationAndPlaceHandler(
-    locationService, positionContext, placeContext
-  );
 
-  return <LocationPermissionView getUserLocationAndPlace={getUserLocationAndPlace} />;
+  return (
+    <div id="noLocation">
+      <p><strong>Assalamu'alaikum</strong>,</p>
+      <p>We cannot show you prayer times without knowing where you are.</p>
+      <br />
+      <button className="primary large shadow" onClick={() => {
+        getUserLocationAndPlaceHandler(locationService, positionContext, placeContext);
+      }}>
+        <Icon icon={crosshair} size={16} />Share my location
+      </button>
+    </div>
+  );
 }
 
 export default LocationPermission;
 
 const getUserLocationAndPlaceHandler = async (locationService: LocationService,
   positionContext: PositionContext, placeContext: PlaceContext) => {
-  let coords = await locationService.getPosition();
-  positionContext.setCoords(coords);
-  await getUserPlaceHandler(locationService, placeContext);
+    let coords = await locationService.getPosition();
+    positionContext.setCoords(coords);
+    await getUserPlaceHandler(locationService, placeContext);
 };
 
 const getUserPlaceHandler = async (locationService: LocationService, placeContext: PlaceContext) => {
